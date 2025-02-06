50772917f637605d1a5eefa979543c977416c871
Matheus Valenzuela <matheuszvalenz@gmail.com>
2025-02-06 11:00:51 +0000

    toolbar component

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -8,10 +8,49 @@ import Table from "@tiptap/extension-table";
 import TableCell from "@tiptap/extension-table-cell";
 import TableHeader from "@tiptap/extension-table-header";
 import TableRow from "@tiptap/extension-table-row";
+import ImageResize from 'tiptap-extension-resize-image';
 import Image from "@tiptap/extension-image";
 
+import { useEditorStore } from "@/store/use-editor-store";
+
 export const Editor = () => {
+
+  const {setEditor} = useEditorStore()
+
   const editor = useEditor({
+    onCreate({editor}) {
+      setEditor(editor)
+        
+    },
+
+    onDestroy(props){
+      setEditor(null);
+    },
+    onUpdate({editor}) {
+      setEditor(editor)
+        
+    },
+    onSelectionUpdate({editor}) {
+      setEditor(editor)
+        
+    },
+    onTransaction({editor}) {
+      setEditor(editor)
+        
+    },
+    onFocus({editor}) {
+      setEditor(editor)
+        
+    },
+    onBlur({editor}) {
+      setEditor(editor)
+        
+    },
+    onContentError({editor}) {
+      setEditor(editor)
+        
+    },
+    
     editorProps: {
       attributes: {
         style: "padding-left: 56px; padding-right: 56px; ",
@@ -22,6 +61,7 @@ export const Editor = () => {
     extensions: [
       StarterKit,
       Image,
+      ImageResize,
       Table,
       TableCell,
       TableHeader,
