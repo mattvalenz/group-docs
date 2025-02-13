cf728888e4d72f3881ab88bc95c74fd0998dc388
Matheus Valenzuela <matheuszvalenz@gmail.com>
2025-02-13 16:44:02 +0000

    creating font size extension for tiptap

--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -21,6 +21,8 @@ import {
   ImageIcon,
   ItalicIcon,
   Link2Icon,
+  ListIcon,
+  ListOrderedIcon,
   ListTodoIcon,
   LucideIcon,
   MessageSquarePlusIcon,
@@ -48,6 +50,49 @@ import { useEditor } from "@tiptap/react";
 import { useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+
+const ListButton = () => {
+  const { editor } = useEditorStore();
+
+  const lists = [
+    {
+      label: "Bullet List",
+      icon: ListIcon,
+      isActive: () => editor?.isActive("bulletList"),
+      onClick : () => editor?.chain().focus().toggleBulletList().run(),
+    },
+    {
+      label: "Ordered List",
+      icon: ListOrderedIcon,
+      isActive: () => editor?.isActive("orderedList"),
+      onClick : () => editor?.chain().focus().toggleOrderedList().run(),
+    },
+    
+  ]
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button className="h-7 min-w-7 shrink-0 flex flex-col items-center justify-center rounded sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm">
+          <ListIcon className="size-4" />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="p-1 flex flex-col gap-y-1 ">
+        {lists.map(({label, icon: Icon, onClick, isActive})=> (
+          <button
+          key={label}
+          onClick={onClick}
+          className={cn("flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80",
+            isActive() && "bg-neutral-200/80"
+          )}>
+            <Icon className="size-4"/>
+            <span className="text-sm">{label}</span>
+          </button>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
 
 const AlignButton = () => {
   const { editor } = useEditorStore();
@@ -483,7 +528,7 @@ export const Toolbar = () => {
       <ImageButton />
       <AlignButton/>
       {/* TODO: Line height */}
-      {/* TODO: List */}
+      <ListButton/>
       {sections[2].map((item) => (
         <ToolbarButton key={item.label} {...item} />
       ))}
