b5783e416858beb0c2b932dfd7e8d834643793c5
Matheus Valenzuela <matheuszvalenz@gmail.com>
2025-02-18 22:59:37 +0000

    working on ruler logic

--- src/app/documents/[documentId]/ruler.tsx
+++ src/app/documents/[documentId]/ruler.tsx
@@ -1,12 +1,54 @@
+import { useRef, useState } from "react";
+import { FaCaretDown } from "react-icons/fa";
+
 const markers = Array.from({ length: 83 }, (_, i) => i);
 
 export const Ruler = () => {
+  const [leftMargin, setLeftMargin] = useState(56);
+  const [rightMargin, setRightMargin] = useState(56);
+
+  const [isDraggingLeft, setIsDraggingLeft] = useState(false);
+  const [isDraggingRight, setIsDraggingRight] = useState(false);
+  const rulerRef = useRef<HTMLDivElement>(null);
+
+  const handleLeftMouseDown = () => {
+    setIsDraggingLeft(true)
+  };
+  const handleRightMouseDown = () => {
+    setIsDraggingRight(true)
+  };
+
+  const handleMouseMove = (e:React.MouseEvent) => {
+    if ((isDraggingLeft || isDraggingRight ) && rulerRef.current) {
+      const container = rulerRef.current.querySelector("#ruler-container");
+    }
+  }
+
   return (
-    <div className="h-6 border-b border-gray-300 flex items-end relative select-none print:hidden">
+    <div 
+    ref={rulerRef}
+    onMouseMove={handleMouseMove}
+    onMouseUp={() => {}}
+    onMouseLeave={() => {}}
+    className="h-6 border-b border-gray-300 flex items-end relative select-none print:hidden">
       <div
         id="ruler-container"
         className="max-w-[816px] mx-auto w-full h-full relative"
       >
+        <Marker
+          position={56}
+          isLeft={true}
+          isDragging={false}
+          onMouseDown={() => {}}
+          onDoubleClick={() => {}}
+        />
+        <Marker
+          position={56}
+          isLeft={false}
+          isDragging={false}
+          onMouseDown={() => {}}
+          onDoubleClick={() => {}}
+        />
         <div className="absolute inset-x-0 bottom-0 h-full">
           <div className="relative h-full w-[816px]">
             {markers.map((marker) => {
@@ -41,24 +83,29 @@ export const Ruler = () => {
   );
 };
 
-
 interface MarkerProps {
-    position: number;
-    isLeft: boolean;
-    isDragging: boolean;
-    onMouseDown: () => void;
-    onDoubleClick: () => void;
-};
+  position: number;
+  isLeft: boolean;
+  isDragging: boolean;
+  onMouseDown: () => void;
+  onDoubleClick: () => void;
+}
 
 const Marker = ({
-    position,
-    isLeft,
-    isDragging,
-    onMouseDown,
-    onDoubleClick
+  position,
+  isLeft,
+  isDragging,
+  onMouseDown,
+  onDoubleClick,
 }: MarkerProps) => {
-    <div className=" absolute top-0 w-4 h-full cursor-ew-resize z-[5] group -ml-2"
-    style= {{ [isLeft ? "left" : "right"] : `${position}px`}}>
-
+  return (
+    <div
+      className=" absolute top-0 w-4 h-full cursor-ew-resize z-[5] group -ml-2"
+      style={{ [isLeft ? "left" : "right"]: `${position}px` }}
+      onMouseDown={onMouseDown}
+      onDoubleClick={onDoubleClick}
+    >
+      <FaCaretDown className="absolute left-1/2 top-0 h-full fill-blue-500 transform -translate-x-1/2" />
     </div>
-}
+  );
+};
