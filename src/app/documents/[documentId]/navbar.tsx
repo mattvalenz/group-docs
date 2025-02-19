eddb698601a6933925c8a75b0179cbd887679fc6
Matheus Valenzuela <matheuszvalenz@gmail.com>
2025-02-19 21:46:00 +0000

    working on Menu bar for navbar

--- src/app/documents/[documentId]/navbar.tsx
+++ src/app/documents/[documentId]/navbar.tsx
@@ -1,5 +1,19 @@
 import Image from "next/image";
 import Link from "next/link";
+import { DocumentInput } from "./document-input";
+
+import {
+    Menubar,
+    MenubarContent,
+    MenubarItem,
+    MenubarMenu,
+    MenubarSeparator,
+    MenubarShortcut,
+    MenubarSub,
+    MenubarSubContent,
+    MenubarSubTrigger,
+    MenubarTrigger
+} from "@/components/ui/menubar"
 
 export const Navbar = () => {
   return (
@@ -9,8 +23,12 @@ export const Navbar = () => {
           <Image src="/logo.svg" alt="Logo" width={36} height={36} />
         </Link>
         <div className="flex flex-col">
-            {/*Document input*/}
-            {/* menubar*/}
+            <DocumentInput/>
+            <div className="flex">
+                <Menubar className="">
+
+                </Menubar>
+            </div>
         </div>
       </div>
     </nav>
