1a9f2558eea8e8a097dd1aefb73bfbff9d7ebe47
Matheus Valenzuela <matheuszvalenz@gmail.com>
2025-02-20 23:22:21 +0000

    working on Search for home screen

--- src/app/(home)/navbar.tsx
+++ src/app/(home)/navbar.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import { SearchInput } from "./search-input";
 
 export const Navbar = () => {
     return(
@@ -11,6 +12,8 @@ export const Navbar = () => {
                 <h3 className="text-xl">Docs</h3>
 
             </div>
+            <SearchInput/>
+            <div/>
         </nav>
     )
 }
