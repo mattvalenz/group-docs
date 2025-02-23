a20579c60814956bfac1a949eb33e5762b2b9d62
Matheus Valenzuela <matheuszvalenz@gmail.com>
2025-02-23 20:05:38 +0000

    added nuqs for SearchAdapter

--- src/app/(home)/search-input.tsx
+++ src/app/(home)/search-input.tsx
@@ -3,10 +3,12 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { SearchIcon, XIcon } from "lucide-react";
-import { useRef, useState } from "react";
+import React, { useRef, useState } from "react";
+import { useSearchParam } from "@/hooks/use-search-param";
 
 export const SearchInput = () => {
-  const [value, setValue] = useState("");
+    const [search, setSearch] = useSearchParam()
+  const [value, setValue] = useState(search);
   const inputRef = useRef<HTMLInputElement>(null);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -15,12 +17,20 @@ export const SearchInput = () => {
 
   const handleClear = () => {
     setValue("")
+    setSearch("")
+    inputRef.current?.blur()
+  }
+
+  const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setSearch(value)
     inputRef.current?.blur()
   }
   
   return (
     <div className="flex-1 flex items-center justify-center">
-      <form className=" relative max-w-[720px] w-full">
+      <form onSubmit={handleSubmit} 
+      className=" relative max-w-[720px] w-full">
         <Input
         value={value}
         onChange={handleChange}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google"
+import { Inter } from "next/font/google";
 import "./globals.css";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
 
 const inter = Inter({
-  subsets:["latin"]
-})
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,10 +19,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={inter.className}
-      >
-        {children}
+      <body className={inter.className}>
+        <NuqsAdapter>{children}</NuqsAdapter>
       </body>
     </html>
   );
