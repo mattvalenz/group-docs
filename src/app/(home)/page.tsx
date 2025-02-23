fff80b0ac9135af58bb65e12313057bf2928d74a
Matheus Valenzuela <matheuszvalenz@gmail.com>
2025-02-23 22:23:34 +0000

    working on schema for convex

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,20 @@
+"use client"
+
 import Link from "next/link";
 import { Navbar } from "./navbar";
 import { TemplateGallery } from "./templates-gallery";
+import { useQuery } from "convex/react";
+import { api } from "../../../convex/_generated/api";
 
 const Home = () => {
+
+  const documents = useQuery(api.documents.get)
+  if (documents === undefined) {
+    return(
+      <p> Loading...</p>
+    )
+  }
+
   return (
     <div className=" flex flex-col min-h-screen ">
       <div className="fixed top-0 left-0 right-0 z-10 h-16 b-white p-4">
@@ -10,6 +22,9 @@ const Home = () => {
       </div>
       <div className="mt-16">
      <TemplateGallery/>
+     {documents?.map((document) => (
+      <span key={document._id}> {document.title}</span>
+     ))}
       </div>
     </div>
   );
