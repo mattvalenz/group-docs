d5883bcb18c66a2451160bc300ea3b5f8da6d464
Matheus Valenzuela <matheuszvalenz@gmail.com>
2025-02-23 22:12:18 +0000

    finished templates, using Convex for DB

---
+++ src/constants/templates.ts
@@ -0,0 +1,30 @@
+export const templates = [
+  { id: "blank", 
+    label: "Blank document", 
+    imageUrl: "/blank-document.svg" 
+},
+  { id: "software-proposal", 
+    label: "Software development proposal", 
+    imageUrl: "/software-proposal.svg" 
+},
+  { id: "project-proposal", 
+    label: "Project Proposal", 
+    imageUrl: "/project-proposal.svg" 
+},
+  { id: "business-letter", 
+    label: "Business letter", 
+    imageUrl: "/business-letter.svg" 
+},
+  { id: "resume", 
+    label: "Resumé", 
+    imageUrl: "/resume.svg" 
+},
+  { id: "cover-letter", 
+    label: "Cover letter", 
+    imageUrl: "/cover-letter.svg" 
+},
+  { id: "letter", 
+    label: "Letter", 
+    imageUrl: "/letter.svg" 
+},
+];
