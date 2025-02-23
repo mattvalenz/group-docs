ff413f3b71b97f2047dd371375b2bdd0d221f7e5
Matheus Valenzuela <matheuszvalenz@gmail.com>
2025-02-23 20:30:44 +0000

    working on creating new doc using templates

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { Navbar } from "./navbar";
+import { TemplateGallery } from "./templates-gallery";
 
 const Home = () => {
   return (
@@ -8,11 +9,7 @@ const Home = () => {
         <Navbar />
       </div>
       <div className="mt-16">
-      Click{" "}
-      <Link href="/documents/123">
-        <span className="text-blue-500 underline">here</span>
-      </Link>{" "}
-      to go to DocumentId
+     <TemplateGallery/>
       </div>
     </div>
   );
