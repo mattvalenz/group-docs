2f664d7b60872eebedd71c33318f79563622e5a6
Matheus Valenzuela <matheuszvalenz@gmail.com>
2025-02-25 22:08:36 +0000

    adding the option to delete documents

--- src/components/remove-dialog.tsx
+++ src/components/remove-dialog.tsx
@@ -1,3 +1,47 @@
 "use client";
 
-import {}
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { Id } from "../../convex/_generated/dataModel";
+
+interface RemoveDialogProps{
+    documentId: Id<"documents">;
+    children: React.ReactNode;
+}
+
+export const RemoveDialog = ({documentId, children}: RemoveDialogProps) => {
+    return(
+        <AlertDialog>
+            <AlertDialogTrigger asChild>
+                {children}
+            </AlertDialogTrigger>
+            <AlertDialogContent onClick={(e) => e.stopPropagation()}>
+                <AlertDialogHeader>
+                    <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+                    <AlertDialogDescription>
+                        This action cannot be undone. This will permanently delete your document.
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                    <AlertDialogCancel onClick={(e) => e.stopPropagation()}>
+                        Cancel
+                    </AlertDialogCancel>
+                    <AlertDialogAction>
+                        Delete
+                    </AlertDialogAction>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
+    )
+
+    
+}
