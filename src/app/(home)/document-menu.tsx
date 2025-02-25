7f222394587a14d73ebe6cc4233f0562fe18414d
Matheus Valenzuela <matheuszvalenz@gmail.com>
2025-02-25 22:13:34 +0000

    \\

--- src/app/(home)/document-menu.tsx
+++ src/app/(home)/document-menu.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { ExternalLinkIcon, MoreVertical } from "lucide-react";
+import { ExternalLinkIcon, MoreVertical, TrashIcon } from "lucide-react";
 import {
   DropdownMenu,
   DropdownMenuItem,
@@ -7,6 +7,7 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Id } from "../../../convex/_generated/dataModel";
+import { RemoveDialog } from "@/components/remove-dialog";
 
 interface DocumentMenuProps {
   documentId: Id<"documents">;
@@ -28,6 +29,13 @@ export const DocumentMenu = ({
       </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent>
+            <RemoveDialog documentId={documentId}>
+                <DropdownMenuItem onSelect={(e) => e.preventDefault()}
+                    onClick={(e) => e.stopPropagation()}>
+                    <TrashIcon className="size-4 mr-2"/>
+                    Remove
+                </DropdownMenuItem>
+            </RemoveDialog>
             <DropdownMenuItem onClick={() => {onNewTab(documentId)}}>
                 <ExternalLinkIcon className="size-4 mr-2"/>
                 Open in a new tab
