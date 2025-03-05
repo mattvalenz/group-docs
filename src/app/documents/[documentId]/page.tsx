ff6aa828ae379ad0ad960089deb9139546343bed
Matheus Valenzuela <matheuszvalenz@gmail.com>
2025-03-05 16:28:17 +0000

    creating error page

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -8,8 +8,11 @@ interface DocumentIdPageProps {
 }
 
 const DocumentIdPage = async ({ params }: DocumentIdPageProps) => {
+
+
   const { documentId } = await params;
   return (
+    
     <div className="min-h-screen bg-[#fafbfd]">
       <div className=" flex flex-col px-4 pt-2 gap-y-2 fixed top-0 left-0 right-0 z-10 bg-[#FAFBFD] print:hidden h-[112px]">
         <Navbar />
