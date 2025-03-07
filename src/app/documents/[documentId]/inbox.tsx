e94e585cbe97ad261a29f59b21a3730c252f776d
Matheus Valenzuela <matheuszvalenz@gmail.com>
2025-03-07 22:44:43 +0000

    completed mentions, finishing notifications

--- src/app/documents/[documentId]/inbox.tsx
+++ src/app/documents/[documentId]/inbox.tsx
@@ -1,57 +1,62 @@
-"use client"
-import { BellIcon } from "lucide-react"
-import { InboxNotification, InboxNotificationList } from "@liveblocks/react-ui"
-import { useInboxNotifications } from "@liveblocks/react/suspense"
-import { ClientSideSuspense } from "@liveblocks/react"
-import { Button } from "@/components/ui/button"
-import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
-
+"use client";
+import { BellIcon } from "lucide-react";
+import { InboxNotification, InboxNotificationList } from "@liveblocks/react-ui";
+import { useInboxNotifications } from "@liveblocks/react/suspense";
+import { ClientSideSuspense } from "@liveblocks/react";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export const Inbox = () => {
-    return(
-        <ClientSideSuspense fallback={null}>
-            <InboxMenu/>
-        </ClientSideSuspense>
-    )
-}
+  return (
+    <ClientSideSuspense
+      fallback={
+        <Button variant={"ghost"}
+        disabled
+         className="relative"
+          size="icon">
+          <BellIcon className="size-5" />
+        </Button>
+      }
+    >
+      <InboxMenu />
+    </ClientSideSuspense>
+  );
+};
 
 const InboxMenu = () => {
-
-    const { inboxNotifications} = useInboxNotifications()
-    return (
-
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                <Button variant={"ghost"}
-                className="relative"
-                size="icon">
-                    <BellIcon className="size-5"/>
-                    {inboxNotifications?.length > 0 && (
-                        <span className="absolute -top-1 -right-1 size-4 rounded-full bg-sky-500 text-xs text-white flex items-center justify-center">
-                            {inboxNotifications.length}
-                        </span>
-                    ) }
-
-                </Button>
-
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end" className="w-auto">
-                {inboxNotifications.length > 0 ? (
-                    <InboxNotificationList>
-                        {inboxNotifications.map((inboxNotification) => (
-                            <InboxNotification
-                             key={inboxNotification.id}
-                             inboxNotification={inboxNotification}/>
-                        ))}
-                    </InboxNotificationList>
-                )
-            : (
-                <div className="p-2 w-[400px] text-center text-sm text-muted-foreground">
-                    No notifications.
-                </div>
-            )}
-
-            </DropdownMenuContent>
-        </DropdownMenu>
-    )
-}
+  const { inboxNotifications } = useInboxNotifications();
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant={"ghost"} className="relative" size="icon">
+          <BellIcon className="size-5" />
+          {inboxNotifications?.length > 0 && (
+            <span className="absolute -top-1 -right-1 size-4 rounded-full bg-sky-500 text-xs text-white flex items-center justify-center">
+              {inboxNotifications.length}
+            </span>
+          )}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-auto">
+        {inboxNotifications.length > 0 ? (
+          <InboxNotificationList>
+            {inboxNotifications.map((inboxNotification) => (
+              <InboxNotification
+                key={inboxNotification.id}
+                inboxNotification={inboxNotification}
+              />
+            ))}
+          </InboxNotificationList>
+        ) : (
+          <div className="p-2 w-[400px] text-center text-sm text-muted-foreground">
+            No notifications.
+          </div>
+        )}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
