20fe2ce76ec9e43138eb06d17c6e3fbf270c2fec
Matheus Valenzuela <matheuszvalenz@gmail.com>
2025-03-10 21:34:08 +0000

    stuck

--- src/app/documents/[documentId]/document-input.tsx
+++ src/app/documents/[documentId]/document-input.tsx
@@ -3,6 +3,8 @@ import { Id } from "../../../../convex/_generated/dataModel";
 import { useRef, useState } from "react";
 import { useMutation } from "convex/react";
 import { api } from "../../../../convex/_generated/api";
+import { useDebounce } from "@/hooks/use-debounce";
+import { toast } from "sonner";
 
 interface DocumentInputProps {
   title: string;
@@ -19,22 +21,54 @@ export const DocumentInput = ({ title, id }: DocumentInputProps) => {
 
   const mutate = useMutation(api.documents.updateById);
 
+  const debouncedUpdate = useDebounce((newValue: string) => {
+    if (newValue === title) return;
+
+    setIsPending(true)
+    mutate({id, title:newValue})
+    .then(() => toast.success("Document updated"))
+    .catch(() => toast.error("Something went wrong"))
+    .finally(() => setIsPending(false))
+    
+  }) 
+
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    
+
+    setIsPending(true)
+    mutate({id, title:value})
+    .then(() => {
+        toast.success("Document updated");
+        setIsEditing(false)
+
+    })
+    .catch(() => toast.error("Something went wrong"))
+    .finally(() => setIsPending(false))
+    
+  }
+
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newValue =e.target.value;
     setValue(newValue);
+    debouncedUpdate(newValue)
     
   }
 
   return (
     <div className="flex items-center gap-2">
       {isEditing ? (
-        <form className="relative w-fit max-w-[50ch]">
+        <form 
+        onSubmit={handleSubmit}
+        className="relative w-fit max-w-[50ch]">
           <span className="invisible whitespace-pre px-1.5 text-lg">
             {value || " "}
             </span>
             <input
               ref={inputRef}
               value={value}
+              onBlur={() => setIsEditing(false)}
               onChange={onChange}
               className="absolute inset-0 text-lg text-black px-1.5 bg-transparent truncate"
             />
